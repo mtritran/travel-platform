2662c647b722de299bf424ce63de0a518ead7ac2
mtritran <tranmanhtri5104@gmail.com>
2026-03-13 10:32:25 +0700

    Update TravelPlatformApplication.java

--- BE/src/main/java/com/mtritran/travelplatform/TravelPlatformApplication.java
+++ BE/src/main/java/com/mtritran/travelplatform/TravelPlatformApplication.java
@@ -1,5 +1,6 @@
 package com.mtritran.travelplatform;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -9,8 +10,10 @@ import java.util.TimeZone;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TravelPlatformApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(TravelPlatformApplication.class, args);
 	}
-
 }
