d96f95df10a81fce781bc6d94659c100ea21a0ee
mtritran <tranmanhtri5104@gmail.com>
2026-03-16 21:31:37 +0700

    Create GuideApplication.java

---
+++ BE/src/main/java/com/mtritran/travelplatform/entity/GuideApplication.java
@@ -0,0 +1,54 @@
+package com.mtritran.travelplatform.entity;
+
+import com.mtritran.travelplatform.enums.ApplicationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "guide_applications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class GuideApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @Column(nullable = false)
+    String idCardUrl;
+
+    @Column(nullable = false)
+    String guideCardUrl;
+
+    @Column(nullable = false)
+    String certificateUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    ApplicationStatus status = ApplicationStatus.PENDING;
+
+    String rejectionReason;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "processed_by")
+    User processedBy;
+}
