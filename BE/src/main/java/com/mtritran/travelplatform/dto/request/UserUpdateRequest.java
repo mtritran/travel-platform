8a117d9a52b1aa9784de488a0884cd726b1df138
mtritran <tranmanhtri5104@gmail.com>
2026-03-16 21:31:41 +0700

    Update UserUpdateRequest.java

--- BE/src/main/java/com/mtritran/travelplatform/dto/request/UserUpdateRequest.java
+++ BE/src/main/java/com/mtritran/travelplatform/dto/request/UserUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.mtritran.travelplatform.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -10,8 +12,14 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserUpdateRequest {
+    @Email(message = "INVALID_EMAIL")
     String email;
+
     String phone;
+
+    @Size(min = 3, message = "INVALID_FULLNAME")
     String fullName;
+
+    @Size(min = 8, message = "INVALID_PASSWORD")
     String password;
 }
