42459447d08434a2fe7a948b1ae6922e2f711f23
mtritran <tranmanhtri5104@gmail.com>
2026-03-16 21:31:47 +0700

    Update UserController.java

--- BE/src/main/java/com/mtritran/travelplatform/controller/UserController.java
+++ BE/src/main/java/com/mtritran/travelplatform/controller/UserController.java
@@ -4,10 +4,12 @@ import com.mtritran.travelplatform.dto.request.UserCreateRequest;
 import com.mtritran.travelplatform.dto.request.UserUpdateRequest;
 import com.mtritran.travelplatform.dto.response.UserResponse;
 import com.mtritran.travelplatform.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,31 +25,36 @@ import java.util.List;
 public class UserController {
     UserService userService;
 
+    @Operation(summary = "Registry new user")
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest request) {
         UserResponse response = userService.createUser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Get all users", description = "Admin only")
     @GetMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<List<UserResponse>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @Operation(summary = "Get user by ID", description = "Admin only")
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<UserResponse> getUserById(@PathVariable String id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    @Operation(summary = "Update user by ID", description = "Admin only")
     @PutMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public ResponseEntity<UserResponse> updateUserById(@PathVariable String id, @RequestBody UserUpdateRequest request) {
+    public ResponseEntity<UserResponse> updateUserById(@PathVariable String id, @Valid @RequestBody UserUpdateRequest request) {
         return ResponseEntity.ok(userService.updateUserById(id, request));
     }
 
+    @Operation(summary = "Delete user by ID", description = "Admin only")
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<Void> deleteUser(@PathVariable String id) {
