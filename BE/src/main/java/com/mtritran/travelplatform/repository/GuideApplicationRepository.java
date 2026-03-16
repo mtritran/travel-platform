35ccb6a08b2bf5c9ea0924cf6ac3a490cff20a72
mtritran <tranmanhtri5104@gmail.com>
2026-03-16 21:31:27 +0700

    Create GuideApplicationRepository.java

---
+++ BE/src/main/java/com/mtritran/travelplatform/repository/GuideApplicationRepository.java
@@ -0,0 +1,17 @@
+package com.mtritran.travelplatform.repository;
+
+import com.mtritran.travelplatform.entity.GuideApplication;
+import com.mtritran.travelplatform.entity.User;
+import com.mtritran.travelplatform.enums.ApplicationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GuideApplicationRepository extends JpaRepository<GuideApplication, String> {
+    List<GuideApplication> findAllByStatus(ApplicationStatus status);
+    List<GuideApplication> findAllByUser(User user);
+    Optional<GuideApplication> findByUser(User user);
+}
