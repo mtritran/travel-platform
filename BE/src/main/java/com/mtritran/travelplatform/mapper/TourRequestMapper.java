f45f80779d0737ed3346b443ac3f81f6dcce7036
mtritran <tranmanhtri5104@gmail.com>
2026-03-17 09:16:44 +0700

    Create TourRequestMapper.java

---
+++ BE/src/main/java/com/mtritran/travelplatform/mapper/TourRequestMapper.java
@@ -0,0 +1,14 @@
+package com.mtritran.travelplatform.mapper;
+
+import com.mtritran.travelplatform.dto.response.TourRequestResponse;
+import com.mtritran.travelplatform.entity.TourRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface TourRequestMapper {
+    @Mapping(target = "userName", source = "user.fullName")
+    @Mapping(target = "locationName", source = "location.name")
+    @Mapping(target = "guideName", source = "guide.fullName")
+    TourRequestResponse toResponse(TourRequest request);
+}
