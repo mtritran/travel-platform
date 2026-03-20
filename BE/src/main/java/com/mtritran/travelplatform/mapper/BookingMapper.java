253566e914447bbd0a8e62f9dd54eef33679394c
mtritran <tranmanhtri5104@gmail.com>
2026-03-20 22:21:50 +0700

    Update BookingMapper.java

--- BE/src/main/java/com/mtritran/travelplatform/mapper/BookingMapper.java
+++ BE/src/main/java/com/mtritran/travelplatform/mapper/BookingMapper.java
@@ -10,5 +10,12 @@ public interface BookingMapper {
     @Mapping(target = "userName", source = "user.fullName")
     @Mapping(target = "tourTitle", source = "tour.title")
     @Mapping(target = "guideName", source = "tour.guide.fullName")
+    @Mapping(target = "pickupLocationName", source = "pickupLocation.name")
+    @Mapping(target = "pickupLocationAddress", source = "pickupLocation.address")
+    @Mapping(target = "pickupLatitude", source = "pickupLocation.latitude")
+    @Mapping(target = "pickupLongitude", source = "pickupLocation.longitude")
+    @Mapping(target = "depositPercentage", source = "tour.depositPercentage")
+    @Mapping(target = "tourStartDate", source = "tour.startDate")
+    @Mapping(target = "tourStartTime", source = "tour.startTime")
     BookingResponse toResponse(Booking booking);
 }
