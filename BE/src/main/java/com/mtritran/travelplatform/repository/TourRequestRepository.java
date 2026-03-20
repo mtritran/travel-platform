acabfdd881a0104abd2e7b06cea17d62bb3745c1
mtritran <tranmanhtri5104@gmail.com>
2026-03-20 22:22:04 +0700

    Update TourRequestRepository.java

--- BE/src/main/java/com/mtritran/travelplatform/repository/TourRequestRepository.java
+++ BE/src/main/java/com/mtritran/travelplatform/repository/TourRequestRepository.java
@@ -12,6 +12,8 @@ import java.util.List;
 @Repository
 public interface TourRequestRepository extends JpaRepository<TourRequest, String> {
     List<TourRequest> findAllByStatus(TourRequestStatus status);
+    List<TourRequest> findAllByUser(com.mtritran.travelplatform.entity.User user);
+    List<TourRequest> findAllByGuide(com.mtritran.travelplatform.entity.User guide);
     
     @Query(value = "SELECT tr.* FROM tour_requests tr " +
             "LEFT JOIN locations l ON tr.location_id = l.id " +
