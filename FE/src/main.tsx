17517d6b64a0a8e0f7b320dd40f71f9237a1ef14
mtritran <tranmanhtri5104@gmail.com>
2026-03-20 22:23:09 +0700

    Update main.tsx

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Polyfill for SockJS
+if (typeof (window as any).global === 'undefined') {
+  (window as any).global = window;
+}
+if (typeof (window as any).process === 'undefined') {
+  (window as any).process = { env: {} };
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
