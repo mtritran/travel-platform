5cae94073ae8b833f317de40b8f7953e7204c2ef
mtritran <tranmanhtri5104@gmail.com>
2026-03-20 22:22:08 +0700

    Update LocationService.java

--- BE/src/main/java/com/mtritran/travelplatform/service/LocationService.java
+++ BE/src/main/java/com/mtritran/travelplatform/service/LocationService.java
@@ -1,6 +1,10 @@
 package com.mtritran.travelplatform.service;
 
+import com.mtritran.travelplatform.dto.request.LocationCreateRequest;
 import com.mtritran.travelplatform.dto.response.LocationResponse;
+import com.mtritran.travelplatform.entity.Location;
+import com.mtritran.travelplatform.exception.AppException;
+import com.mtritran.travelplatform.exception.ErrorCode;
 import com.mtritran.travelplatform.mapper.LocationMapper;
 import com.mtritran.travelplatform.repository.LocationRepository;
 import lombok.AccessLevel;
@@ -21,5 +25,41 @@ public class LocationService {
         return locationRepository.findAll().stream()
                 .map(locationMapper::toResponse)
                 .toList();
+    }
+
+    public LocationResponse createLocation(LocationCreateRequest request) {
+        var existing = locationRepository.findByName(request.getName());
+        if (existing.isPresent()) {
+            return locationMapper.toResponse(existing.get());
+        }
+
+        Location location = Location.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .latitude(request.getLatitude())
+                .longitude(request.getLongitude())
+                .imageUrl(request.getImageUrl())
+                .build();
+        return locationMapper.toResponse(locationRepository.save(location));
+    }
+
+    public LocationResponse updateLocation(String id, LocationCreateRequest request) {
+        Location location = locationRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.LOCATION_NOT_FOUND));
+        
+        location.setName(request.getName());
+        location.setAddress(request.getAddress());
+        location.setLatitude(request.getLatitude());
+        location.setLongitude(request.getLongitude());
+        location.setImageUrl(request.getImageUrl());
+        
+        return locationMapper.toResponse(locationRepository.save(location));
+    }
+
+    public void deleteLocation(String id) {
+        if (!locationRepository.existsById(id)) {
+            throw new AppException(ErrorCode.LOCATION_NOT_FOUND);
+        }
+        locationRepository.deleteById(id);
     }
 }
